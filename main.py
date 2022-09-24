515e2b4692194da2f36f5031f8841841fe083e3b
coltonensz <coltonensz123@gmail.com>
2022-09-24 12:14:50 -0500

    first commit with visual studios

--- main.py
+++ main.py
@@ -21,7 +21,28 @@ plt.plot(msftarray)
 # show the graph
 
 
-plt.savefig('charts/msft.png')
+#plt.savefig('charts/msft.png')
+plt.show()
+#get ignore /charts
+
+
+
+data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")
+
+msftPrices = []
+
+for price in data['Adj Close']:
+    msftPrices.append(price)
+
+print(msftPrices)
+
+msftarray = np.array(msftPrices)
+plt.plot(msftarray)
+
+# show the graph
+
+
+#plt.savefig('charts/msft.png')
 plt.show()
 #get ignore /charts
 
