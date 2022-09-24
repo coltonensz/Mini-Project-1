909ecb362fc8d5fa5643856b15710a24cf21fe6a
coltonensz <coltonensz123@gmail.com>
2022-09-24 12:41:10 -0500

    Added AAPL stock chart

--- main.py
+++ main.py
@@ -17,32 +17,32 @@ print(msftPrices)
 
 msftarray = np.array(msftPrices)
 plt.plot(msftarray)
+plt.title("MSFT")
+
+# show the graph
+
+plt.savefig('charts/msft.png')
+plt.show()
+
+
+#Create AAPL chart
+dataAAPL = yf.download("AAPL", start="2022-08-30", end="2022-09-14")
+
+AAPLPrices = []
+
+for price in dataAAPL['Adj Close']:
+    AAPLPrices.append(price)
+
+print(AAPLPrices)
+
+AAPLarray = np.array(AAPLPrices)
+plt.plot(AAPLarray)
+plt.title("AAPL")
 
 # show the graph
 
 
-#plt.savefig('charts/msft.png')
+plt.savefig('charts/aapl.png')
 plt.show()
-#get ignore /charts
 
-
-
-data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")
-
-msftPrices = []
-
-for price in data['Adj Close']:
-    msftPrices.append(price)
-
-print(msftPrices)
-
-msftarray = np.array(msftPrices)
-plt.plot(msftarray)
-
-# show the graph
-
-
-#plt.savefig('charts/msft.png')
-plt.show()
-#get ignore /charts
 
